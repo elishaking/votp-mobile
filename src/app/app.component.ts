dae9bc7e50d8dd8cf389f1f77553bf7cd8f44082
elishaking <ek.chibueze@gmail.com>
2018-02-14 23:59:01 +0100

    updated dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav, App } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -8,15 +8,40 @@ import { DashoardPage } from '../pages/dashoard/dashoard';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = DashoardPage;
+  @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  rootPage: any = DashoardPage;
+  pages: Array<{ title: string, component: any, icon: string }>;
+  currentPage = [];
+
+  constructor(platform: Platform, statusBar: StatusBar,
+    splashScreen: SplashScreen, private app: App) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    this.pages = [
+      { title: 'Dashboard', component: DashoardPage, icon: "calculator" },
+      // { title: 'History', component: HistoryPage, icon: "list-box" },
+      // { title: 'Matrices', component: MatricesPage, icon: "grid" },
+      // { title: 'Functions', component: FunctionsPage, icon: "pulse" }
+    ];
+
+    this.currentPage[0] = this.pages[0];
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    // console.log(page, this.currentPage[0]);
+    if (page != this.currentPage[0]) {
+      this.currentPage[0] = page;
+      this.nav.setRoot(page.component);
+    }
+    // this.menu.close();
   }
 }
 
